bb28ad29ef8b1389e567cc68ebffc0bff156727d
Aman Sharma <amanantonio1932815@gmail.com>
2024-10-01 03:26:30 +0530

    updated google oauth

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
-import App from './App';
-import {reducers} from './reducers';
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import App from "./App";
+import { reducers } from "./reducers";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import "./index.css";
 
-const store =createStore(reducers,compose(applyMiddleware(thunk)));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App/>
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_CLIENT_ID}`}>
+      <App />
+    </GoogleOAuthProvider>
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
