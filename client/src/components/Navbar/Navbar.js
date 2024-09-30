d548e1889a791d570aa5b4ac07582b02e0fffb34
Aman Sharma <amanantonio1932815@gmail.com>
2024-10-01 00:49:28 +0530

    improvised navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,39 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
-import { Link, useNavigate , useLocation } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import decode from 'jwt-decode';
+import React, { useState, useEffect, useCallback } from "react";
+import { AppBar, Typography, Toolbar, Avatar, Button } from "@material-ui/core";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
 
-import memories from '../../images/memories.png';
-import * as actionType from '../../constants/actionTypes';
-import useStyles from './styles';
+import memories from "../../images/memories.png";
+import * as actionType from "../../constants/actionTypes";
+import useStyles from "./styles";
+
+// Utility function to check token expiration
+const isTokenExpired = (token) => {
+  const decodedToken = decode(token);
+  return decodedToken.exp * 1000 < new Date().getTime();
+};
 
 const Navbar = () => {
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const [user, setUser] = useState(() =>
+    JSON.parse(localStorage.getItem("profile"))
+  );
   const dispatch = useDispatch();
   const location = useLocation();
-  const navigate = useNavigate ();
+  const navigate = useNavigate();
   const classes = useStyles();
 
-  const logout = () => {
+  const logout = useCallback(() => {
     dispatch({ type: actionType.LOGOUT });
-
-    navigate('/auth');
-
+    navigate("/auth");
     setUser(null);
-  };
+  }, [dispatch, navigate]);
 
   useEffect(() => {
     const token = user?.token;
-
-    if (token) {
-      const decodedToken = decode(token);
-
-      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    if (token && isTokenExpired(token)) {
+      logout();
     }
-
-    setUser(JSON.parse(localStorage.getItem('profile')));
-  }, [location]);
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location, logout]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
@@ -43,12 +45,34 @@ const Navbar = () => {
       <Toolbar className={classes.toolbar}>
         {user?.result ? (
           <div className={classes.profile}>
-            <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
-            <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+            <Avatar
+              className={classes.purple}
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className={classes.userName} variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className={classes.logout}
+              color="secondary"
+              onClick={logout}
+            >
+              Logout
+            </Button>
           </div>
         ) : (
-          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
         )}
       </Toolbar>
     </AppBar>
